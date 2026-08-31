05f470a4c0e4b1f02b9793a6f2dee27ad9d28e7b
Paresh Bhinde <paresh.bhinde@gmail.com>
2026-08-31 13:18:52 +0000

    27th Aug Practice

--- string-manipulation-practice.py
+++ string-manipulation-practice.py
@@ -2,3 +2,10 @@ server = "prod-web01"
 
 print(server)
 print(len(server))
+print(server[-1])
+print(server[0:4])
+print(server[5:])
+print(server[0])
+
+print(server.lower())
+print(server.upper())
